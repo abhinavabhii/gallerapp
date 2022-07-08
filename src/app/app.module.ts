e141c09c8b44e75254d3de02a0ee8a10acbc7b96
abhinavabhii <abhinavkrishna4951@gmail.com>
2022-07-08 12:54:12 +0530

    completed totaly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppviewGalleryComponent } from './appview-gallery/appview-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppviewGalleryComponent
   ],
   imports: [
     BrowserModule,
